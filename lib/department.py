9597e962d454d67d54ce0e8099ce9e45f607d96e
felix mwangi <fmwauramwangi@gmail.com>
2025-03-07 16:08:22 +0300

    lib/department.py

--- lib/department.py
+++ lib/department.py
@@ -3,10 +3,30 @@ from __init__ import CURSOR, CONN
 
 class Department:
 
-    def __init__(self, name, location, id=None):
+  """  def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
         self.location = location
 
     def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+        return f"<Department {self.id}: {self.name}, {self.location}>" """
+  @classmethod
+  def create_table(cls):
+        """ Create a new table to persist the attributes of Department instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+  @classmethod
+  def drop_table(cls):
+        """ Drop the table that persists Department instances """
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
